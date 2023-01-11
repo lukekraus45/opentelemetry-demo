fbc951569046879a6d3ffd61dd4c5187a4ddd6a5
Yannick Adam <yannick.adam@datadoghq.com>
2023-01-11 17:43:45 +0100

    Sample RUM implementation - Do not merge

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,12 +1,11 @@
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App, { AppContext, AppProps } from 'next/app';
-import { getCookie } from 'cookies-next';
 import CurrencyProvider from '../providers/Currency.provider';
 import CartProvider from '../providers/Cart.provider';
 import { ThemeProvider } from 'styled-components';
 import Theme from '../styles/Theme';
-import FrontendTracer from '../utils/telemetry/FrontendTracer';
+import { datadogRum } from '@datadog/browser-rum';
 
 declare global {
   interface Window {
@@ -18,10 +17,26 @@ declare global {
   }
 }
 
+/* Remove OTel tracing libraries
 if (typeof window !== 'undefined') {
   const collector = getCookie('otelCollectorUrl')?.toString() || '';
   FrontendTracer(collector);
-}
+}*/
+
+// Use Datadog RUM with tracing propagation
+datadogRum.init({
+  applicationId: '[RUM_APPLICATION_ID]',
+  clientToken: '[RUM_CLIENT_TOKEN]',
+  site: 'datadoghq.com',
+  service:'[RUM_SERVICE_NAME]',
+  allowedTracingUrls: [{match: () => true, propagatorTypes: ['tracecontext'] }],
+  version: '1.0.0',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 100,
+  traceSampleRate: 100,
+  trackUserInteractions: true,
+  defaultPrivacyLevel:'mask-user-input'
+});
 
 const queryClient = new QueryClient();
 
