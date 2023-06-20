881dc0d535402f07448a2b9988f1d2137afb2baf
Dinesh Gurumurthy <dinesh.gurumurthy@datadoghq.com>
2023-06-20 15:45:33 -0400

    remove debug for agent script

--- ci/scripts/ci-deploy-agent.sh
+++ ci/scripts/ci-deploy-agent.sh
@@ -7,7 +7,6 @@
 
 set -euo pipefail
 IFS=$'\n\t'
-set -x
 
 clusterName=$1
 clusterArn=$2
@@ -18,10 +17,10 @@ install_agent() {
   release_name="datadog-agent"
 
   # if repo already exists, helm 3+ will skip
-  helm --debug repo add datadog https://helm.datadoghq.com
+  helm repo add datadog https://helm.datadoghq.com
 
   # --install will run `helm install` if not already present.
-  helm --debug upgrade "${release_name}" -n "${namespace}" datadog/datadog --install \
+  helm upgrade "${release_name}" -n "${namespace}" datadog/datadog --install \
     -f ./ci/datadog-agent-values.yaml --set datadog.apiKey=$DD_API_KEY
 
 }
